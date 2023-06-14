33bf0ffb64330c96dfb7089dba6798bf76c6e011
xvido <xvido@stuba.sk>
2023-06-14 14:18:29 +0200

    Added component for writing

--- Sifri/src/Okno.java
+++ Sifri/src/Okno.java
@@ -1,10 +1,11 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Okno extends JFrame implements ActionListener {
 
-    private JLabel TextFromUser;
+    private JTextField TextFromUser;
     private JLabel TextAfterEncryption;
     private JButton ButtonForEncryption;
     private JButton ButtonForDecryption;
@@ -14,6 +15,14 @@ public class Okno extends JFrame implements ActionListener {
        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        this.setSize(500,500);
 
+       JPanel frame = new JPanel();
+       frame.setLayout(new BorderLayout(3,1));
+
+       TextFromUser = new JTextField("Write text for encryption",JTextField.CENTER);
+
+       frame.add(TextFromUser,BorderLayout.NORTH);
+
+       this.add(frame);
        this.setLocationRelativeTo(null);
        this.setVisible(true);
     }
