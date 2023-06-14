4f10d919ff25e883f24b913045f2ead213a461b7
xvido <xvido@stuba.sk>
2023-06-14 15:25:48 +0200

    Encryption type 1

---
+++ Sifri/src/EncryptionType1.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class EncryptionType1 {
+    private static final Map<Character, Character> encryptionMap;
+    static {
+        encryptionMap = new HashMap<>();
+        encryptionMap.put('a', 'x');
+        encryptionMap.put('b', 'm');
+        encryptionMap.put('c', 'z');
+        encryptionMap.put('d', 'n');
+    }
+    private final JLabel TextAfterEncryption;
+
+    public EncryptionType1(JLabel textAfterEncryption) {
+        TextAfterEncryption = textAfterEncryption;
+    }
+
+    public void performEncryption(String text) {
+        StringBuilder encryptedMessage = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            char encryptedChar = encryptionMap.getOrDefault(Character.toLowerCase(c), c);
+            encryptedMessage.append(encryptedChar);
+        }
+        TextAfterEncryption.setText(encryptedMessage.toString());
+    }
+}
--- Sifri/src/Okno.java
+++ Sifri/src/Okno.java
@@ -5,35 +5,55 @@ import java.awt.event.ActionListener;
 
 public class Okno extends JFrame implements ActionListener {
 
+    private final EncryptionType1 encryptionType1;
     private JTextField TextFromUser;
-    private JLabel TextAfterEncryption;
-    private JButton ButtonForEncryption;
-    private JButton ButtonForDecryption;
-
+    private final JLabel textAfterEncryption;
+    private final JButton ButtonForEncryption;
+    private final JButton ButtonForDecryption;
+    private final JComboBox chooseEncryption;
 
    Okno() {
        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
        this.setSize(500,300);
-
+       String encryptionType[]={"Encryption type 1","Encryption type 2"};
        JPanel frame = new JPanel();
        frame.setLayout(new BorderLayout(3,1));
 
-       TextFromUser = new JTextField("Write text for encryption",JTextField.CENTER);
+       TextFromUser = new JTextField("Write text to encrypt");
        ButtonForDecryption = new JButton("Decryption");
        ButtonForEncryption = new JButton("Encryption");
-       TextAfterEncryption= new JLabel("HAAHAHAHAHAHAHAHA");
+       textAfterEncryption= new JLabel("HAAHAHAHAHAHAHAHA");
+       chooseEncryption = new JComboBox(encryptionType);
 
+       frame.add(chooseEncryption,BorderLayout.CENTER);
        frame.add(TextFromUser,BorderLayout.PAGE_START);
        frame.add(ButtonForEncryption,BorderLayout.LINE_START);
        frame.add(ButtonForDecryption,BorderLayout.LINE_END);
-       frame.add(TextAfterEncryption,BorderLayout.PAGE_END);
+       frame.add(textAfterEncryption,BorderLayout.PAGE_END);
 
+       chooseEncryption.addActionListener(this);
+       ButtonForDecryption.addActionListener(this);
+       ButtonForEncryption.addActionListener(this);
+
+       encryptionType1 = new EncryptionType1 ( textAfterEncryption);
        this.add(frame);
        this.setLocationRelativeTo(null);
        this.setVisible(true);
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==ButtonForDecryption){
 
+        }
+        else if (e.getSource()==ButtonForEncryption){
+            String selectedType = (String) chooseEncryption.getSelectedItem();
+            assert selectedType != null;
+            if(selectedType.equals("Encryption type 1")){
+                encryptionType1.performEncryption(TextFromUser.getText());
+            }
+            else if(selectedType.equals("Encryption type 2")){
+
+            }
+        }
     }
 }
