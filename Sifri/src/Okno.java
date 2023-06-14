a8263cbd3d871e8e5bfa7762bc4d55cb604ff5ee
xvido <xvido@stuba.sk>
2023-06-14 14:26:18 +0200

    Added buttons and label

--- Sifri/src/Okno.java
+++ Sifri/src/Okno.java
@@ -13,14 +13,20 @@ public class Okno extends JFrame implements ActionListener {
 
    Okno() {
        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       this.setSize(500,500);
+       this.setSize(500,300);
 
        JPanel frame = new JPanel();
        frame.setLayout(new BorderLayout(3,1));
 
        TextFromUser = new JTextField("Write text for encryption",JTextField.CENTER);
+       ButtonForDecryption = new JButton("Decryption");
+       ButtonForEncryption = new JButton("Encryption");
+       TextAfterEncryption= new JLabel("HAAHAHAHAHAHAHAHA");
 
-       frame.add(TextFromUser,BorderLayout.NORTH);
+       frame.add(TextFromUser,BorderLayout.PAGE_START);
+       frame.add(ButtonForEncryption,BorderLayout.LINE_START);
+       frame.add(ButtonForDecryption,BorderLayout.LINE_END);
+       frame.add(TextAfterEncryption,BorderLayout.PAGE_END);
 
        this.add(frame);
        this.setLocationRelativeTo(null);
