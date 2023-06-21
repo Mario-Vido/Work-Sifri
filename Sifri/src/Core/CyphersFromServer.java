02ef76dacd786ee5a8cc16a5f578470c499dbc02
xvido <xvido@stuba.sk>
2023-06-21 10:32:13 +0200

    Table servlet and cypherol.jsp

--- Sifri/src/Core/CyphersFromServer.java
+++ Sifri/src/Core/CyphersFromServer.java
@@ -16,32 +16,6 @@ public class CyphersFromServer {
     }
 
     public void getKeysFromCyphers() throws IOException {
-//        keys = new ArrayList<>();
-//        names = new ArrayList<>();
-//        cypherMap = new HashMap<>();
-//
-//        URL url = new URL("http://localhost:8080/creatingcypher");
-//
-//        HttpURLConnection connection;
-//        connection = (HttpURLConnection) url.openConnection();
-//        connection.setRequestMethod("GET");
-//
-//        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//        StringBuilder response = new StringBuilder();
-//        String line;
-//        while ((line = reader.readLine()) != null) {
-//            response.append(line);
-//        }
-//        reader.close();
-//
-//        Gson gson = new Gson();
-//        Cypher[] cyphers = gson.fromJson(response.toString(), Cypher[].class);
-//
-//        for (Cypher cypher : cyphers) {
-//            keys.add(cypher.getKey());
-//            names.add(cypher.getName());
-//            cypherMap.put(cypher.getKey(), cypher);
-//        }
         names = new ArrayList<>();
 
         URL url = new URL("http://localhost:8080/creatingcypher");
